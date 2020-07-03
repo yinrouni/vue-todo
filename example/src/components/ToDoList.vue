ccc7766e6a99316f49ed9165c462d4cd3b2a7d54
rouni.yin <yin.ro@husky.neu.edu>
2020-07-03 02:16:09 -0400

    tasks list

---
+++ example/src/components/ToDoList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="ToDoList">
+    <h1>{{title}}</h1>
+
+    <ul>
+      <li v-for="item in items"
+          :key="item.id"
+      >
+        <span>{{item.title}}</span>
+        <button>Done</button>
+
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDoList',
+  data () {
+    return {
+      title: 'My ToDoList',
+      items: [{id: 1, title: 'task1'}, {id: 2, title: 'task2'},
+        {id: 3, title: 'task3'}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul{
+  list-style-type: none;
+}
+</style>
