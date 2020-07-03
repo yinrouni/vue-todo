8f6c1f204b286da99261551d67b3090858be7482
rouni.yin <yin.ro@husky.neu.edu>
2020-07-03 05:50:11 -0400

    table layout

--- example/src/components/ToDoList.vue
+++ example/src/components/ToDoList.vue
@@ -1,31 +1,57 @@
 <template>
   <div class="ToDoList">
     <h1>{{title}}</h1>
+    <el-row>
+      <el-col :span="2">
+        <el-button icon="el-icon-plus" size="small" @click="addTask">Add task</el-button></el-col>
+    </el-row>
+    <el-table
+      :data="items"
+      style="width: 100%">
+      <el-table-column label="Status">
+        <template slot-scope="scope">
+          <el-button v-bind:type="scope.row.done?'success':'plain'" icon="el-icon-check" size="mini" circle @click="toggleDone(scope.$index, scope.row)"></el-button>
+        </template>
+      </el-table-column>
+      <el-table-column label="Task" >
+        <template slot-scope="scope">
+          <span v-show="!scope.row.isEditing">{{scope.row.title}}</span>
+          <el-input v-show="scope.row.isEditing" v-model="editItem" :placeholder="scope.row.title" autofocus="true"
+                    @change="edit(scope.row)"/>
+        </template>
+      </el-table-column>
 
-        <el-row v-for="item in items"
-                :key="item.id"
-                :gutter="20"
-        >
-          <el-col :span="2">
-        <el-button v-bind:type="item.done?'success':'plain'" icon="el-icon-check" size="mini" circle @click="toggleDone(item)"></el-button>
-          </el-col>
-          <el-col :span="14">
-        <span v-show="!item.isEditing">{{item.title}}</span>
-            <el-input v-show="item.isEditing" v-model="editItem" :placeholder="item.title" autofocus="true"
-            @change="edit(item)"/>
-          </el-col>
-          <el-col :span="2">
-        <el-button type="primary" icon="el-icon-edit" size="mini" circle @click="item.isEditing = !item.isEditing"></el-button>
-          </el-col>
-          <el-col :span="2">
-        <el-button type="danger" icon="el-icon-delete" size="mini" circle @click="toggleDelete(item)"></el-button>
-          </el-col>
-        </el-row>
+      <el-table-column label="Edit">
+        <template slot-scope="scope">
+          <el-button type="primary" icon="el-icon-edit" size="mini" circle @click="scope.row.isEditing = !scope.row.isEditing"></el-button>
+        </template>
+      </el-table-column>
 
-    <div>
-      <el-input type="text" v-model="newItem"/>
-      <el-button @click="addTask">Add</el-button>
-    </div>
+      <el-table-column label="Delete">
+        <template slot-scope="scope">
+          <el-button type="danger" icon="el-icon-delete" size="mini" circle @click="toggleDelete(scope.row)"></el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+<!--      <el-row v-for="item in items"-->
+<!--                :key="item.id"-->
+<!--                :gutter="20"-->
+<!--        >-->
+<!--          <el-col :span="2">-->
+<!--        <el-button v-bind:type="item.done?'success':'plain'" icon="el-icon-check" size="mini" circle @click="toggleDone(item)"></el-button>-->
+<!--          </el-col>-->
+<!--          <el-col :span="14">-->
+<!--        <span v-show="!item.isEditing">{{item.title}}</span>-->
+<!--            <el-input v-show="item.isEditing" v-model="editItem" :placeholder="item.title" autofocus="true"-->
+<!--                                            @change="edit(item)"/>-->
+<!--          </el-col>-->
+<!--          <el-col :span="2">-->
+<!--        <el-button type="primary" icon="el-icon-edit" size="mini" circle @click="item.isEditing = !item.isEditing"></el-button>-->
+<!--          </el-col>-->
+<!--          <el-col :span="2">-->
+<!--        <el-button type="danger" icon="el-icon-delete" size="mini" circle @click="toggleDelete(item)"></el-button>-->
+<!--          </el-col>-->
+<!--        </el-row>-->
 
   </div>
 </template>
@@ -54,11 +80,11 @@ export default {
       })
     },
     addTask () {
-      this.items.push({id: this.newId++, title: this.newItem, done: false, isEditing: false})
+      this.items.push({id: this.newId++, title: this.newItem, done: false, isEditing: true})
       this.newItem = ''
     },
-    toggleDone (item) {
-      item.done = !item.done
+    toggleDone (index, row) {
+      row.done = !row.done
     },
     edit (i) {
       this.items.map((item) => {
@@ -75,7 +101,5 @@ export default {
 </script>
 
 <style scoped>
-  ul {
-    list-style-type: none;
-  }
+
 </style>
