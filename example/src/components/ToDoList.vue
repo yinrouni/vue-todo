55f90043ff8fe987e1e38c86f97c8b375fe09649
rouni.yin <yin.ro@husky.neu.edu>
2020-07-03 03:31:53 -0400

    layout && done

--- example/src/components/ToDoList.vue
+++ example/src/components/ToDoList.vue
@@ -2,19 +2,27 @@
   <div class="ToDoList">
     <h1>{{title}}</h1>
 
-    <ul>
-      <li v-for="item in items"
-          :key="item.id"
-      >
+        <el-row v-for="item in items"
+                :key="item.id"
+                :gutter="20"
+        >
+          <el-col :span="2">
+        <el-button v-bind:type="item.done?'success':'plain'" icon="el-icon-check" size="mini" circle @click="toggleDone(item)"></el-button>
+          </el-col>
+          <el-col :span="14">
         <span>{{item.title}}</span>
-        <button @click="toggleDone(item)">Done</button>
-
-      </li>
-    </ul>
+          </el-col>
+          <el-col :span="2">
+        <el-button type="primary" icon="el-icon-edit" size="mini" circle></el-button>
+          </el-col>
+          <el-col :span="2">
+        <el-button type="danger" icon="el-icon-delete" size="mini" circle @click="toggleDelete(item)"></el-button>
+          </el-col>
+        </el-row>
 
     <div>
-      <input type="text" v-model="newItem">
-      <button @click="addTask">Add</button>
+      <el-input type="text" v-model="newItem"/>
+      <el-button @click="addTask">Add</el-button>
     </div>
 
   </div>
@@ -28,13 +36,13 @@ export default {
       title: 'My ToDoList',
       newItem: '',
       newId: 4,
-      items: [{id: 1, title: 'task1'}, {id: 2, title: 'task2'},
-        {id: 3, title: 'task3'}
+      items: [{id: 1, title: 'task1', done: false}, {id: 2, title: 'task2', done: false},
+        {id: 3, title: 'task3', done: false}
       ]
     }
   },
   methods: {
-    toggleDone (i) {
+    toggleDelete (i) {
       this.items.map((item, index) => {
         if (item.id === i.id) {
           this.items.splice(index, 1)
@@ -42,8 +50,11 @@ export default {
       })
     },
     addTask () {
-      this.items.push({id: this.newId++, title: this.newItem})
+      this.items.push({id: this.newId++, title: this.newItem, done: false})
       this.newItem = ''
+    },
+    toggleDone (item) {
+      item.done = !item.done
     }
   }
 
@@ -51,7 +62,7 @@ export default {
 </script>
 
 <style scoped>
-ul{
-  list-style-type: none;
-}
+  ul {
+    list-style-type: none;
+  }
 </style>
--- example/src/router/index.js
+++ example/src/router/index.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import ToDoList from '../components/ToDoList'
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(Router)
+Vue.use(ElementUI, {locale})
 
 export default new Router({
   routes: [
