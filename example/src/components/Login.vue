bedfe7e89006e2f67b704dae59cdd3d6f3fe1a88
rouni.yin <yin.ro@husky.neu.edu>
2020-07-03 23:36:46 -0400

    login page

---
+++ example/src/components/Login.vue
@@ -0,0 +1,67 @@
+<template>
+  <el-form ref="form" :model="form">
+    <el-form-item label="Username">
+      <el-input v-model="form.username"></el-input>
+    </el-form-item>
+
+    <el-form-item label="Password">
+      <el-input type="password" v-model="form.password"></el-input>
+    </el-form-item>
+
+    <el-button @click="onLogin"> Login </el-button>
+
+  </el-form>
+
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      credentials: [{username: 'yinrouni', password: '123456'}, {username: 'zoe', password: '54321'}],
+      form: {
+        username: '',
+        password: ''
+      }
+
+    }
+  },
+  methods: {
+    onLogin () {
+      let found = false
+      this.credentials.forEach((c) => {
+        if (c.username === this.form.username && c.password === this.form.password) {
+          found = true
+        }
+      })
+
+      if (found) {
+        this.success()
+      } else {
+        this.fail()
+      }
+    },
+
+    success () {
+      this.$message({
+        showClose: true,
+        message: 'Success!',
+        type: 'success'
+      })
+    },
+    fail () {
+      this.$message({
+        showClose: true,
+        message: 'Check your credential...',
+        type: 'error'
+      })
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- example/src/router/index.js
+++ example/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Container from '../components/Container'
+import Login from '../components/Login'
 import ElementUI from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/en'
 import 'element-ui/lib/theme-chalk/index.css'
@@ -12,8 +12,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Container',
-      component: Container
+      name: 'Login',
+      component: Login
     }
   ]
 })
